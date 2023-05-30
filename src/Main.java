809a0a55f0ddfc41366d661f1e40465de7848863
Lenka <aruru.goddes@gmail.com>
2023-05-31 01:29:49 +0300

    Выполнены задачи 1-3

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задача 1. Объявление и инициализация переменных.
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("dog = " + dog);
+        System.out.println("cat = " + cat);
+        System.out.println("paper = " + paper);
+        //Задача 2. Увеличение значений переменных на 4.
+        dog = dog + 4;
+        System.out.println("dog = " + dog);
+        cat = cat + 4;
+        System.out.println("cat = " + cat);
+        paper = paper + 4;
+        System.out.println("paper = " + paper);
+        //Задача 3. Уменьшение значений переменных.
+        dog = dog - 3.5;
+        System.out.println("dog = " + dog);
+        cat = cat - 1.6;
+        System.out.println("cat = " + cat);
+        paper = paper - 7639;
+        System.out.println("paper = " + paper);
+
+    }
+}
